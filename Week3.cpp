5c3cba9a65a7fb108738e0add79c6f61e3d28e93
Berkant <berkantagur6@gmail.com>
2024-03-25 10:51:51 +0300

    first commit

---
+++ Week3.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int array[10];
+    int evenCount = 0;
+    int oddCount = 0;
+
+    // Input numbers
+    for (int i = 0; i < 10; i++)
+    {
+        cout << "Enter Value for " << i << ": ";
+        cin >> array[i];
+    }
+
+    // Count even and odd numbers
+    for (int i = 0; i < 10; i++)
+    {
+
+        if (array[i] % 2 == 0)
+        {
+            evenCount++;
+        }
+        else
+        {
+            oddCount++;
+        }
+    }
+
+    // Display result
+    cout << "Number of even numbers: " << evenCount << endl;
+    cout << "Number of odd numbers: " << oddCount << endl;
+
+    return 0;
+}
