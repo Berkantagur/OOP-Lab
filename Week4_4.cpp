c4979be212e1208e18d539a9c4db86ffd7c6ff02
Berkant <berkantagur6@gmail.com>
2024-03-26 12:02:12 +0300

    .cpp

--- Week4_4.cpp
+++ Week4_4.cpp
@@ -6,10 +6,12 @@ class mystack
 {
     char arrayStack[SIZE];
     int top;
-    public:
-    mystack(); // constructor
+
+public:
+    mystack();          // constructor
     void push(char ch); // push character on stack
-    char pop(); // pop character from stack
+    void display();     // display stack
+    char pop();         // pop character from stack
 };
 
 mystack::mystack()
@@ -29,6 +31,21 @@ void mystack::push(char ch)
     top++;
 }
 
+void mystack::display()
+{
+    if (top == 0)
+    {
+        cout << "Stack is empty\n";
+        return;
+    }
+
+    cout << "Stack content:\n";
+    for (int i = top - 1; i >= 0; i--)
+    {
+        cout << arrayStack[i] << "  ";
+    }
+}
+
 char mystack::pop()
 {
     if (top == 0)
@@ -44,20 +61,29 @@ int main()
 {
     mystack s1, s2; // Create two stacks that are automatically initialized.
     int i;
-    s1.push('a'); s2.push('x');
-    s1.push('b'); s2.push('y');
-    s1.push('c'); s2.push('z');
+    s1.push('a');
+    s2.push('x');
+    s1.push('b');
+    s2.push('y');
+    s1.push('c');
+    s2.push('z');
+    cout << "Stack in s1: ";
+    s1.display();
+    cout << "\n";
+    cout << "Stack in s2: ";
+    s2.display();
+    cout << "\n\n";
     for (i = 0; i < 3; i++)
     {
         cout << "Pop s1: " << s1.pop() << "\n";
     }
-    
+    s1.display();
     cout << "\n";
-
     for (i = 0; i < 3; i++)
     {
         cout << "Pop s2: " << s2.pop() << "\n";
     }
+    s2.display();
 
     return 0;
 }
